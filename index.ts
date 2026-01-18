7d7fa3dfb510545a982cc9c448c10d8063022d04
DogFortune <dogfortune.2000@gmail.com>
2026-01-18 19:16:14 +0900

    add indent setting refs #1

--- index.ts
+++ index.ts
@@ -7,6 +7,6 @@ const folderNames = manifest.folderNames
 const fileNames = manifest.fileNames
 const fileExtensions = manifest.fileExtensions
 
-Bun.write('manifests/folders.json', JSON.stringify(folderNames));
-Bun.write('manifests/files.json', JSON.stringify(fileNames));
-Bun.write('manifests/extensions.json', JSON.stringify(fileExtensions));
+Bun.write('manifests/folders.json', JSON.stringify(folderNames, null, 4));
+Bun.write('manifests/files.json', JSON.stringify(fileNames, null, 4));
+Bun.write('manifests/extensions.json', JSON.stringify(fileExtensions, null, 4));
