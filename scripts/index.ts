947157f13cca6456e2c38c420d73dae854872fc7
DogFortune <dogfortune.2000@gmail.com>
2026-03-05 14:15:45 +0900

    add languageIds refs #17

--- scripts/index.ts
+++ scripts/index.ts
@@ -6,7 +6,9 @@ const manifest = generateManifest();
 const folderNames = manifest.folderNames
 const fileNames = manifest.fileNames
 const fileExtensions = manifest.fileExtensions
+const languageIds = manifest.languageIds
 
 Bun.write('manifests/folders.json', JSON.stringify(folderNames, null, 4));
 Bun.write('manifests/files.json', JSON.stringify(fileNames, null, 4));
 Bun.write('manifests/extensions.json', JSON.stringify(fileExtensions, null, 4));
+Bun.write('manifests/languageIds.json', JSON.stringify(languageIds, null, 4));
